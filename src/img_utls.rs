adaac91d7fdd0b8df4fd7b68c6800944303c1d79
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-04-30 02:14:42 -0400

    switch to actix, and upload files via multipart

--- src/img_utls.rs
+++ src/img_utls.rs
@@ -1,7 +1,11 @@
 use std::process::Command;
-use rocket::Data;
 use rand::{distributions::Alphanumeric, Rng};
 use mongodb::Client;
+use bson::doc;
+use std::io::Write;
+use actix_multipart::Multipart;
+use actix_web::{web, Error};
+use futures::{StreamExt, TryStreamExt};
 
 #[path = "secret.rs"] mod secret;
 
@@ -18,31 +22,48 @@ pub fn label_image(img_path: &str) -> Vec<String> {
     return vec!();
 }
 
-pub fn handle_image_upload(img_data: Data, img_type: &str, uid: &str) -> String {
-    let random_bytes: String = rand::thread_rng()
-                                .sample_iter(&Alphanumeric)
-                                .take(16)
-                                .map(char::from)
-                                .collect();
-    let img_path = format!("uploads/{}_{}.{}",uid,random_bytes,img_type);
-    img_data.stream_to_file(img_path.as_str());
-
-    let img_labels = label_image(img_path.as_str());
-
+pub async fn handle_image_upload(mut payload: Multipart, uid: &str) -> Result<Vec<String>, Error> {
     // MongoDB stuff - do in wrapper later
-    let client = Client::with_uri_str(&secret::DB_S).await?;
+    let client = Client::with_uri_str(&secret::DB_S).await.unwrap();
     let db = client.database("imgboard");
     let coll = db.collection("images");
 
-    let doc = doc! {
-        "img_path": image_path,
+    let mut paths: Vec<String> = vec!();
+
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let random_bytes: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(16)
+            .map(char::from)
+            .collect();
+
+        let content_type = field.content_disposition().unwrap();
+        let filename = content_type.get_filename().unwrap();
+        let filepath = format!("./uploads/{}_{}_{}", uid, random_bytes,sanitize_filename::sanitize(&filename));
+
+        // File::create is blocking operation, use threadpool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
+        }
+
+        let img_path = format!("./uploads/{}_{}_{}", uid, random_bytes,sanitize_filename::sanitize(&filename));
+        let img_labels = label_image(img_path.as_str());
+
+        let doc = doc! {
+        "img_path": img_path.as_str(),
         "lables" : img_labels,
         "uid": uid,
-    };
+        };
 
-    let result = coll.insert_one(doc, None).await?;
-    println!("{:#?}", result);
-
-    return img_path;
-
+        coll.insert_one(doc, None).await.unwrap();
+        println!("{:#?}", img_path);
+        paths.push(img_path);
+    }
+    Ok(paths)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 mod img_utls;
 
-#[macro_use] extern crate rocket;
+use actix_multipart::Multipart;
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 
+async fn save_file(mut payload: Multipart) -> HttpResponse {
+    let response = img_utls::handle_image_upload(payload,"091237").await.unwrap().into_iter().collect::<String>();
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(response)
 }
 
-#[tokio::main]
-async fn main() {
+fn index() -> HttpResponse {
+    let html = r#"<html>
+        <head><title>Upload Test</title></head>
+        <body>
+            <form target="/" method="post" enctype="multipart/form-data">
+                <input type="file" multiple name="file"/>
+                <button type="submit">Submit</button>
+            </form>
+        </body>
+    </html>"#;
 
-    println!("{:?}",img_utls::label_image("a.jpg"));
-    rocket::ignite().mount("/", routes![hello]).launch();
-    // // async {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
 
-    // // };
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    std::fs::create_dir_all("./tmp").unwrap();
 
+    let ip = "0.0.0.0:3000";
+
+    HttpServer::new(|| {
+        App::new().wrap(middleware::Logger::default()).service(
+            web::resource("/")
+                .route(web::get().to(index))
+                .route(web::post().to(save_file)),
+        )
+    })
+        .bind(ip)?
+        .run()
+        .await
 }
