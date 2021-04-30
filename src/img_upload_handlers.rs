9deb6311c23778697bc1cff9a6533fcd51dc00fe
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-04-30 04:15:30 -0400

    added title, changed way files are handled and forms

--- src/img_upload_handlers.rs
+++ src/img_upload_handlers.rs
@@ -1,9 +1,17 @@
-use actix_multipart::Multipart;
-use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
+use actix_multipart::{Multipart, Field};
+use actix_web::{HttpResponse};
+use futures::{TryStreamExt, StreamExt};
+use std::convert::TryInto;
+use std::mem::swap;
+// use mime::Mime;
+
 #[path = "img_utls.rs"] mod img_utls;
 
-pub async fn save_file(mut payload: Multipart) -> HttpResponse {
-    let response = img_utls::handle_image_upload(payload,"091237").await.unwrap().into_iter().collect::<String>();
+pub async fn save_file(mut parts: awmp::Parts) -> HttpResponse {
+    let file_data = parts.files.take("file").pop().unwrap();
+    let title = String::from(*parts.texts.as_hash_map().get("title").unwrap());
+
+    let response = img_utls::handle_image_upload(file_data,title).await.unwrap();
 
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
@@ -13,11 +21,13 @@ pub async fn save_file(mut payload: Multipart) -> HttpResponse {
 pub fn upload_img() -> HttpResponse {
     let html = r#"<html>
         <head><title>Upload Test</title></head>
-        <body>
+        <body><center>
             <form target="/upload" method="post" enctype="multipart/form-data">
-                <input type="file" multiple name="file"/>
+                <label for="title">Picture Title:</label>
+                <input type="text" id="title" name="title"><br><br>
+                <input type="file" name="file" accept="image/png, image/jpeg"/>
                 <button type="submit">Submit</button>
-            </form>
+            </form></center>
         </body>
     </html>"#;
 
--- src/img_utls.rs
+++ src/img_utls.rs
@@ -3,7 +3,7 @@ use rand::{distributions::Alphanumeric, Rng};
 use mongodb::Client;
 use bson::doc;
 use std::io::Write;
-use actix_multipart::Multipart;
+use actix_multipart::{Multipart, Field};
 use actix_web::{web, Error};
 use futures::{StreamExt, TryStreamExt};
 
@@ -22,49 +22,32 @@ pub fn label_image(img_path: &str) -> Vec<String> {
     return vec!();
 }
 
-pub async fn handle_image_upload(mut payload: Multipart, uid: &str) -> Result<Vec<String>, Error> {
+pub async fn handle_image_upload(file_data: awmp::File, title: String) -> Result<String, Error> {
     // MongoDB stuff - do in wrapper later
     let client = Client::with_uri_str(&secret::DB_S).await.unwrap();
     let db = client.database("imgboard");
     let coll = db.collection("images");
 
-    let mut paths: Vec<String> = vec!();
+    let random_bytes: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(16)
+        .map(char::from)
+        .collect();
 
-    while let Ok(Some(mut field)) = payload.try_next().await {
-        let random_bytes: String = rand::thread_rng()
-            .sample_iter(&Alphanumeric)
-            .take(16)
-            .map(char::from)
-            .collect();
+    let filepath = format!("./uploads/{}_{}", random_bytes,file_data.sanitized_file_name());
+    file_data.into_inner().persist(filepath.as_str()).map(|_| filepath.as_str()).unwrap();
 
-        let content_type = field.content_disposition().unwrap();
-        let filename = content_type.get_filename().unwrap();
-        let filepath = format!("./uploads/{}_{}_{}", uid, random_bytes,sanitize_filename::sanitize(&filename));
+    let img_labels = label_image(filepath.as_str());
 
-        // File::create is blocking operation, use threadpool
-        let mut f = web::block(|| std::fs::File::create(filepath))
-            .await
-            .unwrap();
+    let doc = doc! {
+        "img_path": filepath.as_str(),
+        "lables" : img_labels,
+        "title": title,
+        "comments" : doc! {}
+    };
 
-        // Field in turn is stream of *Bytes* object
-        while let Some(chunk) = field.next().await {
-            let data = chunk.unwrap();
-            f = web::block(move || f.write_all(&data).map(|_| f)).await?;
-        }
+    coll.insert_one(doc, None).await.unwrap();
+    println!("{:#?}", filepath);
 
-        let img_path = format!("./uploads/{}_{}_{}", uid, random_bytes,sanitize_filename::sanitize(&filename));
-        let img_labels = label_image(img_path.as_str());
-
-        let doc = doc! {
-            "img_path": img_path.as_str(),
-            "lables" : img_labels,
-            "uid": uid,
-            "comments" : doc! {}
-        };
-
-        coll.insert_one(doc, None).await.unwrap();
-        println!("{:#?}", img_path);
-        paths.push(img_path);
-    }
-    Ok(paths)
+    Ok(filepath)
 }
