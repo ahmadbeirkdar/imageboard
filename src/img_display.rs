c8b7d79e68a268a4ed7b2e3e66523725c0fc1bc4
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-04-30 22:11:10 -0400

    Display comments in image view

--- src/img_display.rs
+++ src/img_display.rs
@@ -8,14 +8,29 @@ use bson::Document;
 
 #[get("/img/{id}")]
 pub async fn image_view(data: web::Data<Mutex<DB>>,web::Path(id): web::Path<String>) -> HttpResponse {
-    match data.lock().unwrap().get_image(&id) {
+
+    match data.get_ref().lock().unwrap().get_image(&id) {
         None => {HttpResponse::Ok().content_type("text/html; charset=utf-8").body("404")}
         Some(doc) => {
             let title = doc.get("title").unwrap().as_str().unwrap();
             let img = format!("/get_img/{}",id);
             let head = static_html::IMAGE_VIEW_HEAD.replace("{Title}",title).replace("{img}",&img);
+            let comments_docs = doc.get("comments").unwrap().as_array().unwrap();
+            let mut comments: Vec<String> = vec!();
+            for i in comments_docs {
+                let doc = i.as_document().unwrap();
+                comments.push(
+                    static_html::get_comment(
+                        doc.get("author").unwrap().as_str().unwrap(),
+                        doc.get("date").unwrap().as_str().unwrap(),
+                        doc.get("text").unwrap().as_str().unwrap()
+                    )
+                )
+            }
+
             HttpResponse::Ok()
-                .content_type("text/html; charset=utf-8").body(format!("{}{}",head, static_html::IMAGE_VIEW_FOOT))
+                .content_type("text/html; charset=utf-8")
+                .body(format!("{}{}{}",head,comments.into_iter().collect::<String>(),static_html::IMAGE_VIEW_FOOT))
         }
     }
 
@@ -23,7 +38,7 @@ pub async fn image_view(data: web::Data<Mutex<DB>>,web::Path(id): web::Path<Stri
 
 #[get("/get_img/{id}")]
 pub async fn show_img(data: web::Data<Mutex<DB>>, web::Path(id): web::Path<String>) -> NamedFile  {
-    return match data.lock().unwrap().get_image(&id) {
+    return match data.get_ref().lock().unwrap().get_image(&id) {
         None => {
             NamedFile::open("./static/placeholder.jpg").unwrap()
         }
@@ -32,3 +47,4 @@ pub async fn show_img(data: web::Data<Mutex<DB>>, web::Path(id): web::Path<Strin
         }
     }
 }
+
